ff3f41b4e836ae03fd6080d8ab7f22daad798dbc
Balaji556 <balajireddi296@gmail.com>
2025-02-16 16:06:12 +0530

    curd operations

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,36 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  private baseUrl = "https://sheetdb.io/api/v1/rpo3o4nae1ren";
+
   constructor(private http: HttpClient) { }
 
   postStudent(data: any) {
-    return this.http.post<any>("http://localhost:8083/api/v1/insert", data)
+    return this.http.post<any>(this.baseUrl, data)
       .pipe(map((res: any) => {
         return res;
-      }))
+      }));
   }
+
   getStudent() {
-    return this.http.get<any>("https://sheetdb.io/api/v1/rpo3o4nae1ren")
+    return this.http.get<any>(this.baseUrl)
       .pipe(map((res: any) => {
         return res;
-      }))
+      }));
   }
- 
+
   deleteStudent(id: string) {
-    return this.http.delete<any>("https://sheetdb.io/api/v1/rpo3o4nae1ren" + id)
+    return this.http.delete<any>(`${this.baseUrl}/id/${id}`)
       .pipe(map((res: any) => {
         return res;
-      }))
+      }));
   }
+
   updateStudent(data: any, id: string) {
-    return this.http.put<any>("https://sheetdb.io/api/v1/rpo3o4nae1ren" + id, data)
+    return this.http.put<any>(`${this.baseUrl}/id/${id}`, data)
       .pipe(map((res: any) => {
         return res;
-      }))
+      }));
   }
 }
--- src/app/student-dashboard/student-dashboard.component.ts
+++ src/app/student-dashboard/student-dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ApiService } from '../shared/api.service';
 import { Studentdashboard } from '../studentdashboard';
+
 @Component({
   selector: 'app-student-dashboard',
   templateUrl: './student-dashboard.component.html',
@@ -10,92 +11,78 @@ import { Studentdashboard } from '../studentdashboard';
 export class StudentDashboardComponent implements OnInit {
   formvalue!: FormGroup;
   studentModelobj: Studentdashboard = new Studentdashboard();
-  studentData !: any;
+  studentData!: any;
   showAdd!: boolean;
   showUpdate!: boolean;
+
   constructor(private formbuilder: FormBuilder, private api: ApiService) { }
 
   ngOnInit(): void {
     this.formvalue = this.formbuilder.group({
-      id:[''],
+      id: [''],
       firstName: [''],
       lastName: [''],
       Branch: [''],
       Year: [''],
       Mobile: [''],
       Email: ['']
-
-    })
+    });
     this.getAllStudents();
   }
+
   clickAddStudent() {
     this.formvalue.reset();
     this.showAdd = true;
     this.showUpdate = false;
   }
+
   postStudentDetails() {
-    this.studentModelobj.id = this.formvalue.value.id;
-    this.studentModelobj.firstName = this.formvalue.value.firstName;
-    this.studentModelobj.lastName = this.formvalue.value.lastName;
-    this.studentModelobj.Branch = this.formvalue.value.Branch;
-    this.studentModelobj.Year = this.formvalue.value.Year;
-    this.studentModelobj.Mobile = this.formvalue.value.Mobile;
-    this.studentModelobj.Email = this.formvalue.value.Email;
+    this.studentModelobj = { ...this.formvalue.value };
 
     this.api.postStudent(this.studentModelobj).subscribe(res => {
-      console.log(res);
-      alert("Student Added Succesfully")
-      let ref = document.getElementById('cancel')
+      alert("Student Added Successfully");
+      let ref = document.getElementById('cancel');
       ref?.click();
       this.formvalue.reset();
       this.getAllStudents();
-    },
-    err => {
+    }, err => {
       alert("Something Went Wrong");
-    })
-
+    });
   }
+
   getAllStudents() {
     this.api.getStudent().subscribe(res => {
       this.studentData = res;
-    })
+    });
   }
-  deleteStudent(row:any){
-this.api.deleteStudent(row.id).subscribe(res=>{
-  alert("Student Deleted");
-  this.getAllStudents();
-})
+
+  deleteStudent(row: any) {
+    this.api.deleteStudent(row.id).subscribe(res => {
+      alert("Student Deleted Successfully");
+      this.getAllStudents();
+    }, err => {
+      alert("Failed to Delete Student");
+    });
   }
 
   onEdit(row: any) {
     this.showAdd = false;
     this.showUpdate = true;
-    this.studentModelobj.id = row.id;
-    this.formvalue.controls['id'].setValue(row.id);
-    this.formvalue.controls['firstName'].setValue(row.firstName);
-    this.formvalue.controls['lastName'].setValue(row.lastName);
-    this.formvalue.controls['Branch'].setValue(row.Branch);
-    this.formvalue.controls['Year'].setValue(row.Year);
-    this.formvalue.controls['Mobile'].setValue(row.Mobile);
-    this.formvalue.controls['Email'].setValue(row.Email);
+    this.studentModelobj = { ...row }; // Copy row data properly
+    this.formvalue.patchValue(row); // Efficiently update form fields
   }
 
   updateStudent() {
-    this.studentModelobj.id = this.formvalue.value.id;
-    this.studentModelobj.firstName = this.formvalue.value.firstName;
-    this.studentModelobj.lastName = this.formvalue.value.lastName;
-    this.studentModelobj.Branch = this.formvalue.value.Branch;
-    this.studentModelobj.Year = this.formvalue.value.Year;
-    this.studentModelobj.Mobile = this.formvalue.value.Mobile;
-    this.studentModelobj.Email = this.formvalue.value.Email;
+    this.studentModelobj = { ...this.formvalue.value };
 
-    this.api.updateStudent(this.studentModelobj, this.studentModelobj.id)
-      .subscribe(res => {
-        alert("Updated Successfully");
-        let ref = document.getElementById('cancel')
-        ref?.click();
-        this.formvalue.reset();
-        this.getAllStudents();
-      })
+    this.api.updateStudent(this.studentModelobj, this.studentModelobj.id).subscribe(res => {
+      alert("Updated Successfully");
+      let ref = document.getElementById('cancel');
+      ref?.click();
+      this.formvalue.reset();
+      this.getAllStudents();
+    }, err => {
+      alert("Failed to Update Student");
+    });
   }
 }
